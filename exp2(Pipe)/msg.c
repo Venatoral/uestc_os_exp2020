418d75a8289d3f26f29f366d4778e2eebc9942b3
Venatoral <15882190474@163.com>
2020-05-18 10:52:25 +0800

    update exp2

---
+++ exp2(Pipe)/msg.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+#define DEBUG
+#undef DEBUG
+
+typedef struct _msg {
+    long type;
+    char body[256];
+}msg;
+int main() {
+    // get key_t for msgget
+    int fid = ftok("./", 0);
+    // create ipc
+    int mid;
+    if ( (mid = msgget(fid, 0666|IPC_CREAT) ) == -1 ) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+    
+    // fork a thread to write msg into ipc
+    int pid = fork();
+    if(pid == 0) {
+        msg* m = calloc(1, sizeof(msg));
+        strcpy(m->body, "Hello, I have written this msg into msgq,can you see it?\0");
+        m->type = 1;
+#ifdef DEBUG
+    printf("before send msg\n");
+#endif
+        if ( msgsnd(mid, m, sizeof(msg), 0) == -1 ) {
+            perror("msgsnd");
+            exit(EXIT_FAILURE);
+        }
+
+#ifdef DEBUG
+    printf("after send msg\n");
+#endif
+        free(m);
+        exit(EXIT_SUCCESS);
+    }
+    // wait child to finish write
+    waitpid(pid, NULL, 0);
+    msg* recv = calloc(1, sizeof(msg));
+#ifdef DEBUG
+    printf("before receive from msgq\n");
+#endif
+    if ( msgrcv(mid, recv, sizeof(msg), 1, 0) == -1 ) {
+        perror("msgrcv");
+        exit(EXIT_FAILURE);
+    }
+#ifdef DEBUG
+    printf("after receive from msgq\n");
+#endif
+    printf("\033[1;31mRecv: %s\n", recv->body);
+    // remove ipc
+    if (msgctl(mid, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
---
+++ exp2(Pipe)/pipe.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+// true on DEBUG mod to see the run track
+// #define DEBUG
+int main() {
+    int fd[2];
+    // Create pipe
+    if ( pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    // child  1 write
+    int pid = fork();
+    if(pid == 0) {
+        char buf[128] = "Child 1 is sending a message\0";
+        close(fd[0]);
+        if( write(fd[1], buf, strlen(buf)) == -1 ) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+#ifdef DEBUG
+    printf("before wait child2\n");
+#endif
+    // wait child 1 to write
+    waitpid(pid, NULL, 0);
+#ifdef DEBUG
+    printf("after wait child2\n");
+#endif
+    int newFd[2];
+    // Create pipe
+    if (pipe(newFd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    // child  2 write
+    pid = fork();
+    if (pid == 0) {
+        char buf[128] = "Child 2 is sending a message\0";
+        close(fd[0]);
+        if (write(newFd[1], buf, strlen(buf)) == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    // wait child 2 to finish write
+#ifdef  DEBUG
+    printf("before wait child2...\n");
+#endif
+    waitpid(pid, NULL, 0);
+#ifdef DEBUG
+    printf("after wait child2...\n");
+#endif
+    // Read message from child1 and child2
+    char msg1[128], msg2[128];
+    // read child 1
+    close(fd[1]);
+#ifdef DEBUG
+    printf("Before read from child1\n");
+#endif
+    if (read(fd[0], msg1, 128) == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+#ifdef DEBUG
+    printf("After read from child1\n");
+#endif
+    // read child2
+#ifdef DEBUG
+    printf("Before read from child2\n");
+#endif
+    if (read(newFd[0], msg2, 128) == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+#ifdef DEBUG
+    printf("After read from child2\n");
+#endif
+
+// show msg1 and msg2;
+printf("\033[1;31mReceive msg:\nmsg1:%s\nmsg2%s\n", msg1, msg2);
+return 0;
+}
